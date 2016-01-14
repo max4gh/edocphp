9f5a85373b89e1390ac0bfb188227343f75f8ed9
Max <51796875@qq.com>
2016-01-14 17:39:14 +0800

    添二级模型演示

---
+++ Application/Home/Model/Sub/SubModel.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Home\Model\Sub;
+/**
+ * 演示二级模型
+ *
+ * @author Max
+ *
+ */
+class SubModel {
+	/**
+	 * 测试二级模型方法
+	 * @param string $param 参数说明
+	 */
+	public function test($param) {
+		return $param;
+	}	
+}
